3d424d8e9a82b05a608e0e84b352cbd7d621f9e1
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-27 17:21:28 +0100

    Initial Commit

---
+++ SimpleIOSApp/SimpleIOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleIOSAppApp.swift
+//  SimpleIOSApp
+//
+//  Created by Fabian on 27.01.21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleIOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
