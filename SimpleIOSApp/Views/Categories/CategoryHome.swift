bffdaa30d1a2b62d5f0a678dc4d7ca242fadb8c9
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-28 18:04:09 +0100

    added base files from previous tutorials

---
+++ SimpleIOSApp/Views/Categories/CategoryHome.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryHome.swift
+//  SimpleIOSApp
+//
+//  Created by Fabian on 28.01.21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
---
+++ SimpleIOSApp/Views/Helpers/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  SimpleIOSApp
+//
+//  Created by Fabian on 28.01.21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var image: Image
+
+    var body: some View {
+        image
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage(image: Image("turtlerock"))
+    }
+}
