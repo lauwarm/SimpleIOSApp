9f6b725dcd3586e58b621624ad1459f9f9f65dc4
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-01-29 15:52:39 +0100

    track the page in a swiftUI view's state

--- SimpleIOSApp/Views/PageView/PageView.swift
+++ SimpleIOSApp/Views/PageView/PageView.swift
@@ -9,15 +9,20 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
+    @State private var currentPage = 0
     
     var body: some View {
-        PageViewController(pages: pages)
+        PageViewController(pages: pages, currentPage: $currentPage)
     }
 }
 
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
-        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
-            .aspectRatio(3 / 2, contentMode: .fit)
+        Group {
+            PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+                .aspectRatio(3 / 2, contentMode: .fit)
+            PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+                .aspectRatio(3 / 2, contentMode: .fit)
+        }
     }
 }
